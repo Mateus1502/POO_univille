0aea0bba13796eeff2b1c823dc3e33cd253896d9
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2025-03-08 13:02:49 -0300

    Delete src directory

--- src/EX1.java
+++
@@ -1,9 +0,0 @@
-public class EX1 {
-    public static void main(String[] args) {
-        int soma =0;
-        for(int x=1;x<1001;x++){
-            soma +=x;
-        }
-        System.out.println(soma);
-    }
-}
