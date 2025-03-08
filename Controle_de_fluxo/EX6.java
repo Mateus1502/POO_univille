251a0eec7e4552938cdf807f88e5ae6236ed2133
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2025-03-08 13:04:41 -0300

    Controle de fluxo

---
+++ Controle_de_fluxo/EX6.java
@@ -0,0 +1,14 @@
+public class EX6 {
+    public static void main(String[] args){
+        for(int i =1;i<=10;i++){
+            System.out.println("Tabuada do "+i);
+            for(int j=1;j<=10;j++){
+                System.out.println(i+"X"+j+"="+(i*j));
+            }
+            System.out.println("-----------------------");
+            System.out.println("FIM");
+        }
+
+    }
+}
+
