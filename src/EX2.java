ca8d762c0bb9fbcbde8ffceb7cacc293f394d308
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2025-03-16 13:46:06 -0300

    Delete src directory

--- src/EX2.java
+++
@@ -1,9 +0,0 @@
-public class EX2 {
-    public static void main(String[] args) {
-        System.out.println("Args"+args.length);
-        for(int i =0;i< args.length;i++){
-            System.out.println(args[i]+" ");
-        }
-        System.out.println();
-    }
-}
